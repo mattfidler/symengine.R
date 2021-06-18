140a053ea2ceda5dae98261f3244744565255d32
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-06-18 12:56:37 -0500

    remove debugging statements

--- inst/sed.R
+++ inst/sed.R
@@ -1,14 +1,9 @@
 source("tmp.R")
 
-print(getwd())
-
 .in <- suppressWarnings(readLines("src/Makevars.in"))
-print(.in)
 .in <- gsub("@@@PKG_LIBS@@@", PKG_LIBS_VALUE, .in)
 .in <- gsub("@@@PKG_CPPFLAGS@@@", PKG_CPPFLAGS_VALUE, .in)
 .in <- gsub("@@@DO_UPSTREAM@@@", DO_UPSTREAM, .in)
-
-print(.in)
 
 file.out <- file("src/Makevars", "wb")
 writeLines(.in, file.out)
